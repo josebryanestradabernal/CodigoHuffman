5729faa07ede6ed577acc6a08160dcf289296769
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-11-17 22:11:06 -0600

    empezando interfaz para terminal

---
+++ src/CompresionDescompresion.c
@@ -0,0 +1,9 @@
+#include<Huffman.h>
+int main (int contVector,char **vector){
+  upercase(vector);
+  if (strcmp(vector[1],"C")){}
+  if (strcmp(vector[1],"D")){}
+  if (strcmp(vector[1],"H")){
+    printf("Compresor y Descompresor Huffman\n")
+  }
+}
