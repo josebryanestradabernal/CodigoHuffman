08e4a64041d2b1afc59022588cc9f3228df8ade9
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-11-18 19:02:50 -0600

    Opciones D,C,H mensaje de error

--- src/CompresionDescompresion.c
+++ src/CompresionDescompresion.c
@@ -1,9 +1,17 @@
-#include<Huffman.h>
+#include<stdio.h>
+#include<string.h>
 int main (int contVector,char **vector){
-  upercase(vector);
-  if (strcmp(vector[1],"C")){}
-  if (strcmp(vector[1],"D")){}
-  if (strcmp(vector[1],"H")){
-    printf("Compresor y Descompresor Huffman\n")
+  if (!strcmp(vector[1],"C")){}
+  else if (!strcmp(vector[1],"D")){}
+  else if (!strcmp(vector[1],"H")){
+    printf("Compresor y Descompresor Huffman\n");
+    printf("Creado por Jose Bryan Estrada Bernal\nBienvenido a la Ayuda\n");
+    printf("Forma de uso: OPCION ARCHIVO\nEjemplo: Huffman D comprimido.cpm\n\n");
+    printf("\tOPCION:\nC         -Comprimir-\nD         -Descomprimir-\n");
+  }
+  else {
+    printf("Error: ");
+    for (int i=1;i<contVector;i++)printf("%s ",vector[i]);
+    printf("\nOrden no encontrada\nH   -Para pedir ayuda-\n" );
   }
 }
