dbba3a1df242bf8f068ba7dbc5e1c4ab43875000
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-11-29 03:31:11 -0600

    version final

--- src/Huffman.h
+++ src/Huffman.h
@@ -1,30 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef __HUFFMAN_H__
+#define __HUFFMAN_H__
 
-//estructuras que se usaran
 
-typedef struct nodo
-{
-   char letra;             /* Letra a la que hace referencia el nodo */
-   unsigned long int bits; /* Valor de la codificación de la letra */
-   char nbits;             /* Número de bits de la codificación */
-   nodo *cero;            /* Puntero a la rama cero de un árbol */
-   nodo *uno;             /* Puntero a la rama uno de un árbol */
-} tNodo;
-typedef struct _nodo
-{
-   char letra;      /* Letra a la que hace referencia el nodo */
-   int frecuencia;  /* veces que aparece la letra en el texto o las letras */
-                    /* de los nodos de las ramas cero y uno */
-   _nodo *sig;      /* Puntero a siguiente nodo de una lista enlazada */
-   _nodo *cero;     /* Puntero a la rama cero de un árbol */
-   _nodo *uno;      /* Puntero a la rama uno de un árbol */
-} tipoNodo;
-typedef struct _tabla
-{
-   char letra;      /* Letra a la que hace referencia el nodo */
-   unsigned long int bits; /* Valor de la codificación de la letra */
-   char nbits;      /* Número de bits de la codificación */
-   _tabla *sig;     /* Siguiente elemento de la tabla */
-} tipoTabla;
-tipoTabla *Tabla;
+#define TAMLIMITE 100
+
+struct nodoArbol {
+	char dato;
+	unsigned freq;
+	struct nodoArbol *izq, *der;
+};
+
+struct listaNodo {
+	unsigned tam;
+	unsigned capacidad;
+	struct nodoArbol** array;
+};
+#endif
