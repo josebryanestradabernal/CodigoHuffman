3018a82cad9f55cd1091c13ca05640250bf445a4
josebryanestradabernal <josebryan.amarciano.estradaber@gmail.com>
2019-11-29 04:46:04 -0600

    agregandofunciones

--- src/Huffman.h
+++ src/Huffman.h
@@ -15,4 +15,19 @@ struct listaNodo {
 	unsigned capacidad;
 	struct nodoArbol** array;
 };
+struct nodoArbol* nuevoNodo(char dato, unsigned freq);
+struct listaNodo* crearLista(unsigned capacidad);
+void intercambiaNodo(struct nodoArbol** a,struct nodoArbol** b);
+void ordenar(struct listaNodo* Lista, int idx);
+int TamUno(struct listaNodo* Lista);
+struct nodoArbol* Min(struct listaNodo* Lista);
+void InsertaLista(struct listaNodo* Lista,struct nodoArbol* nodoArbol);
+void crearNodolista(struct listaNodo* Lista);
+void imprimirArray(int arr[], int n);
+int Hoja(struct nodoArbol* root);
+struct listaNodo* creandoListaNodo(char dato[], int freq[], int tam);
+struct nodoArbol* crearArbol(char dato[], int freq[], int tam);
+void imprimirCodigos(struct nodoArbol* root, int arr[], int top);
+void codigoHuffman(char dato[], int freq[], int tam);
+
 #endif
